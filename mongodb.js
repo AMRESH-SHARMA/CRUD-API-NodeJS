cd6ef7e2cc845729992043bbcde1dd1d068d57c4
AMRESH <95286212+AMRESH-SHARMA@users.noreply.github.com>
2022-08-01 10:32:53 +0530

    db & collection name change

--- mongodb.js
+++ mongodb.js
@@ -2,12 +2,12 @@ const { MongoClient } = require("mongodb");
 
 // Replace the uri string with your connection string.
 const uri ="mongodb://localhost:27017";
-const databaseName = "abc";
+const databaseName = "DT";
 const client = new MongoClient(uri);
 
 async function dbConnect() {
     let result = await client.connect();
     db= result.db(databaseName);
-    return db.collection("abcd");
+    return db.collection("Events");
   }
   module.exports = dbConnect;
